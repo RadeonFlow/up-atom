ff364f91c72882432fdcd84615b00fe544c5c7dd
jpy794 <jpy794@hotmail.com>
2026-05-16 18:10:50 +0000

    fix dpa fused moe

--- atom/model_ops/topK.py
+++ atom/model_ops/topK.py
@@ -66,6 +66,15 @@ def is_rocm_aiter_fusion_shared_expert_enabled_for_quant_config(
                 return False
             break
 
+    dp_size = config.parallel_config.data_parallel_size
+    use_mori_all2all = (
+        dp_size > 1
+        and _has_module("mori")
+        and config.enable_dp_attention
+        and config.enable_expert_parallel
+    )
+    if use_mori_all2all:
+        return False
     return True
 
 
